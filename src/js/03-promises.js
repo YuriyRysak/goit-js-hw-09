a1d82136bd9f110e48065e8f9fd923e595a48fc4
YuriyRysak <yuriy.risak@gmail.com>
2022-11-27 20:21:02 +0200

    Summary

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,35 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+  const makeOrder = dish => {
+    const DELAY = 1000;
+
+    const promise = new Promise((resolve, reject) => {
+        const passed = Math.random() > 0.5;
+        setTimeout(() => {
+            if (passed) {
+                resolve(`✅Вот ваше блюдо: ${dish}`);
+            }
+            reject('❌Извините закончились продукты(');
+        }, DELAY);
+    });
+    return promise;
+   };
+makeOrder('пирожок').then(onMakeOrderSuccess).catch(onMakeOrderError);
+function onMakeOrderSuccess(result) {
+    console.log('onMakeOrderSuccess');
+    console.log(result);
+} 
+function onMakeOrderError(error) {
+    console.log('onMakeOrderError');
+    console.log(error);
+}
