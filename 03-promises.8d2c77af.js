ba9193270b5a927510d3ee9db120db9e39e6c546
YuriyRysak <95884807+YuriyRysak@users.noreply.github.com>
2022-11-28 10:42:28 +0000

    Deploying to gh-pages from @ YuriyRysak/goit-js-hw-09@7d4c18d8ef69fc8a73dd7f528e742e54c5a24efa 🚀

--- 03-promises.8d2c77af.js
+++
@@ -1,2 +0,0 @@
-!function(){var o;(o="пирожок",new Promise((function(n,c){var e=Math.random()>.5;setTimeout((function(){e&&n("✅Вот ваше блюдо: ".concat(o)),c("❌Извините закончились продукты(")}),1e3)}))).then((function(o){console.log("onMakeOrderSuccess"),console.log(o)})).catch((function(o){console.log("onMakeOrderError"),console.log(o)}))}();
-//# sourceMappingURL=03-promises.8d2c77af.js.map
--- 03-promises.bd8d76b7.js
+++
@@ -1,2 +0,0 @@
-var o;(o="пирожок",new Promise(((e,n)=>{const c=Math.random()>.5;setTimeout((()=>{c&&e(`✅Вот ваше блюдо: ${o}`),n("❌Извините закончились продукты(")}),1e3)}))).then((function(o){console.log("onMakeOrderSuccess"),console.log(o)})).catch((function(o){console.log("onMakeOrderError"),console.log(o)}));
-//# sourceMappingURL=03-promises.bd8d76b7.js.map
